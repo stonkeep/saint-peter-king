a173277f8ced7cf3881b8e9f2d63b4cdc9347ffe
Mateus Galasso <stonkeep@users.noreply.github.com>
2017-12-20 16:09:42 -0200

    teste de cliente com endereço, somente backend

--- tests/Unit/ClienteTest.php
+++ tests/Unit/ClienteTest.php
@@ -14,10 +14,24 @@ class ClienteTest extends TestCase
 {
     use DatabaseMigrations;
 
+
     public function setUp()
     {
         parent::setUp();
     }
+
+
+    private $user;
+    public function criaUsuario()
+    {
+        //Cria Usuáiro
+        factory(User::class)->create();
+        //Recupera usuário criado
+        $this->user = User::first();
+        //Verifica se o usuário existe mesmo
+        $this->assertNotNull($this->user);
+    }
+
 
     /**
      * A basic test example.
@@ -25,7 +39,7 @@ class ClienteTest extends TestCase
      * @return void
      * @test
      */
-    public function criaCliente()
+    public function testaCriaCliente()
     {
         factory(Cliente::class)->create();
         $cliente = Cliente::first();
@@ -60,15 +74,10 @@ class ClienteTest extends TestCase
     /** @test */
     public function testaCrudComUsuario()
     {
-        //Cria Usuáiro
-        factory(User::class)->create();
-        //Recupera usuário criado
-        $user = User::first();
-        //Verifica se o usuário existe mesmo
-        $this->assertNotNull($user);
+        $this->criaUsuario();
 
         //Cria um cliente para o novo usuário
-        $user->cliente()->create([
+        $this->user->cliente()->create([
             'cpf_cnpj' => 11111111111,
             'nome' => "Jose",
             'telefone' => 123456789,
@@ -76,7 +85,7 @@ class ClienteTest extends TestCase
 
         $cliente = Cliente::first();
 
-        $this->assertEquals($user->name, $cliente->user->name);
+        $this->assertEquals( $this->user->name, $cliente->user->name);
 
         //Testa UPDATE
         $cliente->nome = "Arthur";
@@ -86,10 +95,100 @@ class ClienteTest extends TestCase
         $this->assertEquals("Arthur", $cliente->nome);
 
         //Testa Delete
-        $user->cliente()->delete();
+         $this->user->cliente()->delete();
         //Tenta achar o cliente no banco de dados
         $cliente = Cliente::first();
         //Assert que não achou
         $this->assertNull($cliente);
     }
+
+
+    /**
+     * @test
+    */
+
+    public function testaCrudComEndereco()
+    {
+        //Cria Usuáiro
+        factory(User::class)->create();
+        //Recupera usuário criado
+         $this->user = User::first();
+        //Verifica se o usuário existe mesmo
+        $this->assertNotNull( $this->user);
+
+
+        //Cria um cliente para o novo usuário
+        $this->user->cliente()->create([
+            'cpf_cnpj' => 11111111111,
+            'nome' => "Jose",
+            'telefone' => 123456789,
+        ]);
+        $cliente = Cliente::first();
+
+        //Verifica se nome do usuário vinculado ao cliente é o mesmo
+        $this->assertEquals( $this->user->name, $cliente->user->name);
+
+        //Crio um endereço novo vinculando o mesmo ao novo cliente
+        $cliente->enderecos()->create([
+            'nome' => "Endereço 1",
+            'cidade' => "Brasília",
+            'logradouro' => "Quadra 111",
+            'cep' => 71900000,
+            'numero' => 9,
+            'Complemento' => "perto de alguma coisa"
+        ]);
+
+        //Busca novamente o cliente no banco de dados para ter certeza
+        $cliente = Cliente::first();
+        //Como o relacionamento retorna um array de objeto, chamo somente um registro retornando assim somente o objeto
+        $this->assertEquals($cliente->enderecos->find(1)->nome, "Endereço 1");
+
+        //Aqui verificase se o endereço gravado no banco de dados é o mesmo que esta com o cliente
+        $endereco = Endereco::first();
+        $this->assertEquals($cliente->enderecos->find(1)->id, $endereco->id);
+
+        //UPDATE do endereço gravado no banco
+        $endereco->nome = "Endereço 2";
+        $endereco->save();
+
+        //Valida se o endereço foi mesmo alterado
+        $endereco = Endereco::first();
+        $this->assertEquals($endereco->nome, "Endereço 2");
+
+        //DELETE do endereço do cliente
+        $endereco->delete();
+
+        //Valida se o endereço foi corretamente deletado
+        $this->assertTrue($endereco->trashed());
+
+        //Busca endereços que não estão deletados
+        $endereco = Endereco::all();
+
+        //Valida se não foi achado nada
+        $this->assertEmpty($endereco);
+
+        //Agora busca endereços que foram deletados com softdelete
+        $endereco = Endereco::withTrashed();
+
+        //Valida a existencia do endereço no banco
+        $this->assertNotEmpty($endereco);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
