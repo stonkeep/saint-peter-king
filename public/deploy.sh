f7f7859e8b748e937890baf4ca33f262639f3c16
stonkeep <stonkeep@users.noreply.github.com>
2018-01-11 23:13:43 -0200

    ajuste deploy

--- public/deploy.sh
+++ public/deploy.sh
@@ -1,6 +1,7 @@
 git reset --hard 
 git pull origin master
-composer install --optimize-autoloader 
+composer install --optimize-autoloader
+npm install
 php artisan migrate:fresh --seed
 php artisan optimize
 php artisan config:cache
